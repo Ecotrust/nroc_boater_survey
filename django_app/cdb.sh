a3e85bb7f8f1086bea8e21427c7295b11cb20f6a
Tim Welch <tim.j.welch@gmail.com>
2010-04-25 00:18:59 +0000

    add intro page

--- django_app/cdb.sh
+++ django_app/cdb.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-DB="or-non-con"
+DB="rbsw"
 
 export PYTHONPATH=$PYTHONPATH:/usr/local/django-trunk/:/usr/local/django-apps/:.
 
 createdb -T gis_template $DB
-psql -d $DB -f ../database/or_coast_cities.sql
-psql -d $DB -f ../database/or_coast_placemarks.sql
 python manage.py syncdb --noinput
