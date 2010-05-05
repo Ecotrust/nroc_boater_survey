f03270c28032c74ae00e29553bbfcb2b3a0c5cef
Tim Welch <tim.j.welch@gmail.com>
2010-05-05 00:49:44 +0000

    require paramaters to access the intro view as well

--- django_app/views.py
+++ django_app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponse
 
 def intro(request):
+    if not request.GET.has_key('id'):    
+        return HttpResponse('We\'re sorry, this section is not accessible without providing the proper information' , status=500)
     interview_id  = request.GET.get('id')
     vessel  = request.GET.get('vessel')
     request.session['interview_id'] = interview_id
