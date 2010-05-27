37bc9627393a02cb5a87eea9ecb3b1e6e18aa731
Ryan Hodges <rhodges@ecotrust.org>
2010-05-27 21:33:22 +0000

    switched to char fields, set primary key in status, cleaned out needless storing of duplicate user_id, month, and user_type status

--- django_app/draw_app/models.py
+++ django_app/draw_app/models.py
@@ -4,156 +4,102 @@ from django.contrib.gis.admin import OSMGeoAdmin
 from django.contrib.auth.models import User
 from django.conf import settings
 import datetime
-
-
-# class OrCoastCities(Model):
-    # gid = IntegerField(primary_key = True)
-    # city = CharField(max_length=25)
-    # the_geom = PointField(srid=4326)        
-    # objects = GeoManager()
-    # class Meta:
-        # db_table = u'or_coast_cities'
         
-# class OrCoastPlacemarks(Model):
-    # gid = IntegerField(primary_key = True)
-    # name = CharField(max_length=25)
-    # type = CharField(max_length=25)
-    # the_geom = PointField(srid=4326)        
-    # objects = GeoManager()
-    # class Meta:
-        # db_table = u'or_coast_placemarks'        
-
-# class Activity(Model):
-    # DrawTypeChoices = (
-        # ( 'point', 'point' ),
-        # ( 'polygon', 'polygon' ),
-        # ( 'none', 'none' )
-    # )    
-    # kn_id = CharField( max_length=20 )
-    # draw_type = CharField( max_length=20, choices=DrawTypeChoices, default='none' )
-    # name = CharField( max_length=100 )    
-    # class Meta:
-        # db_table = u'activities'    
-
-    # def __unicode__(self):v
-        # return unicode('%s : %s' % (self.draw_type, self.name))
-
-# class ActivityRecord(Model):
-    # kn_user_id = TextField()
-    # act = ForeignKey(Activity)    
-    # creation_date = DateTimeField(default=datetime.datetime.now)
-
-    # objects = GeoManager()    
-    # class Meta:
-        # abstract = True
-        # managed = False
-    
-    # def __unicode__(self):
-        # return unicode('%s : %s' % (self.kn_user_id, self.act))
-    # def activity(self):
-        # return unicode('%s' % self.act.name)            
-
 class Route(Model):
-    survey_id = TextField()
-    user_type = CharField( max_length=1 )
-    user_id = TextField()
-    month = TextField()
+    survey_id = CharField(max_length=12)
     geometry = LineStringField(srid=settings.SERVER_SRID)
     objects = GeoManager()
     creation_date = DateTimeField(default=datetime.datetime.now)
     class Meta:
         db_table = u'route'
         
-class RouteAdmin(OSMGeoAdmin):
-    list_display = ('survey_id', 'user_type', 'user_id', 'month', 'creation_date')
-    odering = ('survey_id',)
-    
 class RouteFactor(Model):
     route = ForeignKey(Route)
-    survey_id = TextField()
+    survey_id = CharField(max_length=12)
     route_pk = IntegerField()
-    factor = TextField()
+    factor = CharField(max_length=150)
     class Meta:
         db_table = u'route_factors'
         
-class RouteFactorAdmin(admin.ModelAdmin):
-    list_display = ('survey_id', 'route_pk', 'factor')
-    odering = ('survey_id',)        
-        
 class ActivityArea(Model):
-    survey_id = TextField()
-    user_type = CharField( max_length=1 )
-    user_id = TextField()
-    month = TextField()
+    survey_id = CharField(max_length=12)
     geometry = PolygonField(srid=settings.SERVER_SRID)
-    primary_activity = TextField( blank=True, null=True )
-    duration = TextField( blank=True, null=True )
-    rank = TextField( blank=True, null=True )
-    alternate_activity_type = TextField( blank=True, null=True )
+    primary_activity = CharField( blank=True, null=True, max_length=150 )
+    duration = CharField( blank=True, null=True, max_length= 100 )
+    rank = CharField( blank=True, null=True, max_length=100 )
+    alternate_activity_type = CharField( blank=True, null=True, max_length=150 )
     objects = GeoManager()
     creation_date = DateTimeField(default=datetime.datetime.now)
     class Meta:
         db_table = u'act_area'
         
-class ActivityAdmin(OSMGeoAdmin):
-    list_display = ('survey_id', 'user_type', 'user_id', 'month', 'creation_date')
-    odering = ('survey_id',)
-        
 class ActivityFactor(Model):
     activity = ForeignKey(ActivityArea)
-    survey_id = TextField()
+    survey_id = CharField(max_length=12)
     activity_pk = IntegerField()
-    factor = TextField()
+    factor = CharField(max_length=150)
     class Meta:
         db_table = u'activity_factors'
         
-class ActivityFactorAdmin(admin.ModelAdmin):
-    list_display = ('survey_id', 'activity_pk', 'factor')
-    odering = ('survey_id',)                
-    
 class AltActArea(Model):
-    survey_id = TextField()
-    user_type = CharField( max_length=1 )
-    user_id = TextField()
-    month = TextField()
+    survey_id = CharField(max_length=12)
     geometry = PolygonField(srid=settings.SERVER_SRID)
     preferred_area = ForeignKey(ActivityArea)
-    primary_activity = TextField( blank=True, null=True )
+    primary_activity = CharField( blank=True, null=True, max_length=150 )
     objects = GeoManager()
     creation_date = DateTimeField(default=datetime.datetime.now)
     class Meta:
         db_table = u'alt_act_area'
         
-class AltActAdmin(OSMGeoAdmin):
-    list_display = ('survey_id', 'user_type', 'user_id', 'month', 'creation_date')
-    odering = ('survey_id',)        
-        
 class SurveyStatus(Model):
-    survey_id = TextField()
+    survey_id = CharField(primary_key = True, max_length=12)
     user_type = CharField( max_length=1 )
-    user_id = TextField()
-    month = TextField()
+    user_id = CharField(max_length=6)
+    month = CharField(max_length=2)
     start_time = DateTimeField(default=datetime.datetime.now)
-    map_status = TextField( blank=True, null=True ) #skipped?  May need to be handled before accessing draw_app
-    act_status = TextField( blank=True, null=True )
+    map_status = CharField( blank=True, null=True, max_length=30 ) #skipped?  May need to be handled before accessing draw_app
+    act_status = CharField( blank=True, null=True, max_length=30 )
     complete = BooleanField( default=False )
     complete_time = DateTimeField( blank=True, null=True )
     class Meta:
         db_table = u'survey_status'
+        
+### ADMINS ###        
+        
+class RouteAdmin(OSMGeoAdmin):
+    list_display = ('survey_id', 'creation_date')
+    odering = ('survey_id',)
+    default_lon = -7870000
+    default_lat = 5165000
+    default_zoom = 8
+    mouse_position = True
+    display_srid = True    
     
-
-# class ActivityAdmin(admin.ModelAdmin):
-    # list_display = ('kn_id', 'draw_type', 'name')
-    # ordering = ('id',)
-
-# class ActivityRecordAdmin(OSMGeoAdmin):
-    # list_display = ('kn_user_id','creation_date','activity')
-    # ordering = ('kn_user_id',)
-    # default_lon = -13900000
-    # default_lat = 5520000
-    # default_zoom = 6
-    # mouse_position = True
-    # display_srid = True    
+class RouteFactorAdmin(admin.ModelAdmin):
+    list_display = ('survey_id', 'route_pk', 'factor')
+    odering = ('survey_id',)        
+        
+class ActivityAdmin(OSMGeoAdmin):
+    list_display = ('survey_id', 'creation_date')
+    odering = ('survey_id',)
+    default_lon = -7870000
+    default_lat = 5165000
+    default_zoom = 8
+    mouse_position = True
+    display_srid = True 
+        
+class ActivityFactorAdmin(admin.ModelAdmin):
+    list_display = ('survey_id', 'activity_pk', 'factor')
+    odering = ('survey_id',)                
+    
+class AltActAdmin(OSMGeoAdmin):
+    list_display = ('survey_id', 'creation_date')
+    odering = ('survey_id',)
+    default_lon = -7870000
+    default_lat = 5165000
+    default_zoom = 8
+    mouse_position = True
+    display_srid = True
     
 admin.site.register(Route,RouteAdmin)
 admin.site.register(ActivityArea,ActivityAdmin)
