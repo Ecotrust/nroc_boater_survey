2f4d7f4bf06517627bf1d2818e8d94f416d3047b
Ryan Hodges <rhodges@ecotrust.org>
2010-05-28 19:20:30 +0000

    removed variable passing - now relies on session

--- django_app/views.py
+++ django_app/views.py
@@ -5,22 +5,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from draw_app.models import SurveyStatus
 
 def intro(request):
-    interview_id  = request.GET.get('id')
-    vessel  = request.GET.get('vessel')
-    month = request.GET.get('month')
-    request.session['interview_id'] = interview_id
-    request.session['vessel'] = vessel
-    request.session['month'] = month
-    context = RequestContext(request)
     
     if request.GET.has_key('continue'):
         return HttpResponseRedirect('/draw/')
     elif request.GET.has_key('skip'):
-        skipped_survey = SurveyStatus.objects.get_or_create(survey_id=interview_id)
+        skipped_survey = SurveyStatus.objects.get_or_create(survey_id=request.session['interview_id'])
         #Save skip status here
-        skipped_survey[0].user_type=interview_id[0]
-        skipped_survey[0].user_id=interview_id[1:7]
-        skipped_survey[0].month=interview_id[-2:]
+        skipped_survey[0].user_type=request.session['interview_id'][0]
+        skipped_survey[0].user_id=request.session['interview_id'][1:7]
+        skipped_survey[0].month=request.session['interview_id'][-2:]
         skipped_survey[0].map_status = 'Skipped'
         skipped_survey[0].act_status = 'Skipped'
         skipped_survey[0].save()
@@ -29,6 +22,13 @@ def intro(request):
     if not request.GET.has_key('id'):    
         return HttpResponse('We\'re sorry, this section is not accessible without providing the proper information' , status=500)
 
+    interview_id  = request.GET.get('id')
+    vessel  = request.GET.get('vessel')
+    month = request.GET.get('month')
+    request.session['interview_id'] = interview_id
+    request.session['vessel'] = vessel
+    request.session['month'] = month
+    context = RequestContext(request)
     
     return render_to_response('intro.html', {'interview_id':interview_id,'vessel':vessel,'month':month})
 
