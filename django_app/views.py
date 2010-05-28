68f0703753f4e3142ec548fb0795de7f19d9394e
Ryan Hodges <rhodges@ecotrust.org>
2010-05-28 17:52:17 +0000

    Skipping the survey now creates a status record

--- django_app/views.py
+++ django_app/views.py
@@ -2,16 +2,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect
 
+from draw_app.models import SurveyStatus
+
 def intro(request):
-    if request.GET.has_key('continue'):
-        return HttpResponseRedirect('/draw/')
-    elif request.GET.has_key('skip'):
-        #Save skip status here
-        return HttpResponseRedirect('http://www.maboatersurvey.com/thanks.htm')
-
-    if not request.GET.has_key('id'):    
-        return HttpResponse('We\'re sorry, this section is not accessible without providing the proper information' , status=500)
-
     interview_id  = request.GET.get('id')
     vessel  = request.GET.get('vessel')
     month = request.GET.get('month')
@@ -19,6 +12,24 @@ def intro(request):
     request.session['vessel'] = vessel
     request.session['month'] = month
     context = RequestContext(request)
+    
+    if request.GET.has_key('continue'):
+        return HttpResponseRedirect('/draw/')
+    elif request.GET.has_key('skip'):
+        skipped_survey = SurveyStatus.objects.get_or_create(survey_id=interview_id)
+        #Save skip status here
+        skipped_survey[0].user_type=interview_id[0]
+        skipped_survey[0].user_id=interview_id[1:7]
+        skipped_survey[0].month=interview_id[-2:]
+        skipped_survey[0].map_status = 'Skipped'
+        skipped_survey[0].act_status = 'Skipped'
+        skipped_survey[0].save()
+        return HttpResponseRedirect('http://www.maboatersurvey.com/thanks.htm')
+
+    if not request.GET.has_key('id'):    
+        return HttpResponse('We\'re sorry, this section is not accessible without providing the proper information' , status=500)
+
+    
     return render_to_response('intro.html', {'interview_id':interview_id,'vessel':vessel,'month':month})
 
 def test(request):    
