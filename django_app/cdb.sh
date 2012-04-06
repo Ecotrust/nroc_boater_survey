1b5d1b42c14e56207447f711a02a769f43db047b
Edwin Knuth <eknuth@ecotrust.org>
2012-04-06 09:49:32 -0700

    different header for intro

--- django_app/cdb.sh
+++ django_app/cdb.sh
@@ -6,5 +6,5 @@ DB="nroc_rbs"
 
 export PYTHONPATH=$PYTHONPATH:/usr/local/src/django/:.
 
-createdb -U nroc_rbs -h localhost -W nroc_rbs -T template_postgis $DB
+createdb nroc_rbs -T template_postgis $DB
 python manage.py syncdb --noinput
