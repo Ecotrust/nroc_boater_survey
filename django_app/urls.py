945daf09aa0e91ff5ff724ed17bc390196d918c0
Ryan Hodges <rhodges@ecotrust.org>
2012-04-09 17:41:16 -0700

    Adding install-media as an acceptable url

--- django_app/urls.py
+++ django_app/urls.py
@@ -24,6 +24,7 @@ if settings.DEBUG is True:
     urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
         (r'^admin-media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.ADMIN_MEDIA_ROOT, 'show_indexes': True}),
+        (r'^install-media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.STATICFILES_ROOT, 'show_indexes': True}),
         (r'^admin/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.ADMIN_MEDIA_PREFIX, 'show_indexes': True})
     )
     
