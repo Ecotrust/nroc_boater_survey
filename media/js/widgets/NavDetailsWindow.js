7e5097b00216d011ba0b43cfd46a5617d27d7d68
Ryan Hodges <rhodges@ecotrust.org>
2012-04-18 10:17:06 -0700

    More styles for nav details panel

--- media/js/widgets/NavDetailsWindow.js
+++ media/js/widgets/NavDetailsWindow.js
@@ -8,14 +8,14 @@ gwst.widgets.NavDetailsWindow = Ext.extend(Ext.Window, {
 		Ext.apply(this, {          
             title: 'Map Navigation Details',
         	layout:'fit',
-            width:350,
+            width:450,
             html: '<p>Detailed Instructions:</p>\
             <ol>\
                 <li><b>Move Map</b> - Click on the blue arrow buttons on the screen, click and drag with your mouse, or use your keyboard arrows to center the map over your starting location.</li>\
                 <li><b>Zoom Map</b> - Zoom in and out by clicking the \'+\' and \'-\' buttons on the map or use the scroll wheel on your mouse if you have one.</li>\
                 <li>Please get as close as you can to your starting point, then click the Continue button below.</li>\
             </ol>',
-            height:500,
+            height:300,
             plain: true,
             bodyStyle: 'padding: 8px; font-weight: bold',
             closeAction:'hide',
