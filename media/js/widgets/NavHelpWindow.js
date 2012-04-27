b1078e3d0ec39e4213e9f31fa1a6b46a2520593f
Ryan Hodges <rhodges@ecotrust.org>
2012-04-27 14:28:15 -0700

    Update style and content of Orientation and Navigation windows

--- media/js/widgets/NavHelpWindow.js
+++ media/js/widgets/NavHelpWindow.js
@@ -12,31 +12,41 @@ gwst.widgets.NavHelpWindow = Ext.extend(Ext.Window, {
             title: 'Map Tool Orientation',
         	layout:'fit',
             width:500,
-            html: '<table class="help-table"><tr><td colspan=3><p><b>Map Navigation Buttons</b></p></td></tr>\
+            html: '<table class="help-table"><tr><td colspan=3><p class="help-win"><b>Map Navigation Buttons</b></p></td></tr>\
                 <tr id="nav-help-row">\
                     <td><img src="/media/img/arrow_up-left.png" />\
-                    </td><td>Use these controls to move and zoom the map.</td>\
+                    </td><td><p class="help-win">Use these controls to move and zoom the map.</p></td>\
                     <td style="width: 85px">\
                         <img style="width:39px; height:52px" src="/media/img/nav_arrows.png" />\
                         <img style="width:39px; height:52px" src="/media/img/nav_zoombar.png" />\
                     </td>\
                 </tr>\
                 <tr>\
-                    <td colspan=3><p><b>White Directions Panel</b></p></td>\
+                    <td colspan=3><p class="help-win"><b>White Directions Panel</b></p></td>\
                 </tr>\
                 <tr id="directions-help-row">\
                     <td><img src="/media/img/arrow_left.png" /></td>\
-                    <td>Instructions on what to do are given in this panel to the left of the map at each step of the way.</td>\
+                    <td><p class="help-win">Instructions on what to do are given in this panel to the left of the map at each step of the way.</p></td>\
                     <td>\
                         <img style="width:39px; height:52px; border: 1px solid Gray" src="/media/img/west_panel.png" />\
                     </td>\
                 </tr>\
                 <tr>\
-                    <td colspan=3><p><b>Help Bar</b></p></td>\
+                    <td colspan=3><p class="help-win"><b>Map Layer Switcher</b></p></td>\
+                </tr>\
+                <tr id="layer-help-row">\
+                    <td><img src="/media/img/arrow_up-right.png" /></td>\
+                    <td><p class="help-win">Select different layers to change which base map is shown.</p></td>\
+                    <td>\
+                        <img border: 1px solid Gray" src="/media/img/layers.png" />\
+                    </td>\
+                </tr>\
+                <tr>\
+                    <td colspan=3><p class="help-win"><b>Help Bar</b></p></td>\
                 </tr>\
                 <tr id="help-bar-help-row">\
                     <td><img src="/media/img/arrow_down.png" />\
-                    </td><td>If more help is needed, please refer to the information bar at the bottom of the screen</td>\
+                    </td><td><p class="help-win">If more help is needed, please refer to the information bar at the bottom of the screen.</p></td>\
                     <td></td>\
                 </tr></table>',
             plain: true,
@@ -48,7 +58,7 @@ gwst.widgets.NavHelpWindow = Ext.extend(Ext.Window, {
             bbar: [
 				{xtype:'tbfill'},
 				{
-                    text: 'View detailed instructions',
+                    text: 'View detailed navigation instructions',
                     handler: this.instructionsBtnClicked.createDelegate(this)
                 },
                 {
