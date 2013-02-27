3902fc0e29a83c3539cf3d2bdf60e04c0a637731
Tim Glaser <tim.glaser@ecotrust.org>
2013-02-27 15:43:50 -0800

    added a puppet script for the production environment

--- django_app/cdb.sh
+++ django_app/cdb.sh
@@ -6,5 +6,5 @@ DB="marco_rbs"
 
 export PYTHONPATH=$PYTHONPATH:/usr/local/src/django/:.
 
-createdb -U nroc_rbs -h localhost -W marco_rbs -T template_postgis $DB
+createdb -U marco_rbs -h localhost -W marco_rbs -T template_postgis $DB
 python manage.py syncdb --noinput
