32918a8ae154645ef03ddf62b9cb761fc1249f51
Tim Glaser <tim.glaser@ecotrust.org>
2013-03-01 23:13:52 -0800

    adding third-party folder

---
+++ media/third-party/ext-ux/map/GMapPanel.js
@@ -0,0 +1,191 @@
+/*!
+ * Ext JS Library 3.1.0
+ * Copyright(c) 2006-2009 Ext JS, LLC
+ * licensing@extjs.com
+ * http://www.extjs.com/license
+ */
+/**
+ * @class Ext.ux.map.GMapPanel
+ * @extends Ext.Panel
+ * @author Shea Frederick
+ */
+
+Ext.namespace('Ext.ux.map');
+
+Ext.ux.map.GMapPanel = Ext.extend(Ext.Panel, {
+    initComponent : function(){
+        
+        var defConfig = {
+        	id: "mappanel",
+            plain: true,
+            zoomLevel: 3,
+            yaw: 180,
+            pitch: 0,
+            zoom: 0,
+            gmapType: 'map',
+            border: false
+        };
+        
+        Ext.applyIf(this,defConfig);        
+        Ext.ux.map.GMapPanel.superclass.initComponent.call(this);        
+    },
+    afterRender : function(){
+        
+        var wh = this.ownerCt.getSize();
+        Ext.applyIf(this, wh);
+        
+        Ext.ux.map.GMapPanel.superclass.afterRender.call(this);    
+        if (GBrowserIsCompatible()) {        	
+        	this.gmap = new GMap2(this.body.dom);
+        	this.gmap.setCenter(new GLatLng(44.1427, -121.6076), 7);
+        	this.gmap.setUIToDefault();        	
+        	GEvent.bind(this.gmap, 'load', this, this.onMapReady);
+        }
+
+    },
+    onMapReady : function(){
+
+        //this.addMarkers(this.markers);
+        //this.addMapControls();
+        //this.addOptions();  
+    },
+    onResize : function(w, h){
+
+        if (typeof this.getMap() == 'object') {
+            this.gmap.checkResize();
+        }
+        
+        Ext.ux.map.GMapPanel.superclass.onResize.call(this, w, h);
+
+    },
+    setSize : function(width, height, animate){
+        
+        if (typeof this.getMap() == 'object') {
+            this.gmap.checkResize();
+        }
+        
+        Ext.ux.map.GMapPanel.superclass.setSize.call(this, width, height, animate);
+        
+    },
+    getMap : function(){
+        
+        return this.gmap;
+        
+    },
+    getCenter : function(){
+        
+        return this.getMap().getCenter();
+        
+    },
+    getCenterLatLng : function(){
+        
+        var ll = this.getCenter();
+        return {lat: ll.lat(), lng: ll.lng()};
+        
+    },
+    addMarkers : function(markers) {
+        
+        if (Ext.isArray(markers)){
+            for (var i = 0; i < markers.length; i++) {
+                var mkr_point = new GLatLng(markers[i].lat,markers[i].lng);
+                this.addMarker(mkr_point,markers[i].marker,false,markers[i].setCenter, markers[i].listeners);
+            }
+        }
+        
+    },
+    addMarker : function(point, marker, clear, center, listeners){
+        
+        Ext.applyIf(marker,G_DEFAULT_ICON);
+
+        if (clear === true){
+            this.getMap().clearOverlays();
+        }
+        if (center === true) {
+            this.getMap().setCenter(point, this.zoomLevel);
+        }
+
+        var mark = new GMarker(point,marker);
+        if (typeof listeners === 'object'){
+            for (evt in listeners) {
+                GEvent.bind(mark, evt, this, listeners[evt]);
+            }
+        }
+        this.getMap().addOverlay(mark);
+
+    },
+    addMapControls : function(){
+        
+        if (this.gmapType === 'map') {
+            if (Ext.isArray(this.mapControls)) {
+                for(i=0;i<this.mapControls.length;i++){
+                    this.addMapControl(this.mapControls[i]);
+                }
+            }else if(typeof this.mapControls === 'string'){
+                this.addMapControl(this.mapControls);
+            }else if(typeof this.mapControls === 'object'){
+                this.getMap().addControl(this.mapControls);
+            }
+        }
+        
+    },
+    addMapControl : function(mc){
+        
+        var mcf = window[mc];
+        if (typeof mcf === 'function') {
+            this.getMap().addControl(new mcf());
+        }    
+        
+    },
+    addOptions : function(){
+        
+        if (Ext.isArray(this.mapConfOpts)) {
+            var mc;
+            for(i=0;i<this.mapConfOpts.length;i++){
+                this.addOption(this.mapConfOpts[i]);
+            }
+        }else if(typeof this.mapConfOpts === 'string'){
+            this.addOption(this.mapConfOpts);
+        }        
+        
+    },
+    addOption : function(mc){
+        
+        var mcf = this.getMap()[mc];
+        if (typeof mcf === 'function') {
+            this.getMap()[mc]();
+        }    
+        
+    },
+    geoCodeLookup : function(addr) {
+        
+        this.geocoder = new GClientGeocoder();
+        this.geocoder.getLocations(addr, this.addAddressToMap.createDelegate(this));
+        
+    },
+    addAddressToMap : function(response) {
+        
+        if (!response || response.Status.code != 200) {
+            Ext.MessageBox.alert('Error', 'Code '+response.Status.code+' Error Returned');
+        }else{
+            place = response.Placemark[0];
+            addressinfo = place.AddressDetails;
+            accuracy = addressinfo.Accuracy;
+            if (accuracy === 0) {
+                Ext.MessageBox.alert('Unable to Locate Address', 'Unable to Locate the Address you provided');
+            }else{
+                if (accuracy < 7) {
+                    Ext.MessageBox.alert('Address Accuracy', 'The address provided has a low accuracy.<br><br>Level '+accuracy+' Accuracy (8 = Exact Match, 1 = Vague Match)');
+                }else{
+                    point = new GLatLng(place.Point.coordinates[1], place.Point.coordinates[0]);
+                    if (typeof this.setCenter.marker === 'object' && typeof point === 'object'){
+                        this.addMarker(point,this.setCenter.marker,this.setCenter.marker.clear,true, this.setCenter.listeners);
+                    }
+                }
+            }
+        }
+        
+    }
+ 
+});
+
+Ext.reg('gmappanel', Ext.ux.map.GMapPanel); 
